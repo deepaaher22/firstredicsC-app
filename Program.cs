ae7754a2553bd2f9143203c2b954db184a1f820a
dipaAher22 <dipaliaher2014Gmail.com>
2024-10-13 13:59:36 +0530

    add new one

--- Program.cs
+++ Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Added new changes");
+            Console.WriteLine("feature update ");
+
         }
     }
 }
--- singleton.cs
+++ singleton.cs
@@ -10,3 +10,4 @@ namespace firstredicsC_app
     {
     }
 }
+m m 
